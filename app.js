a5634b894fbdb219e5e4c98e560c61071c97c02c
Alok Mishra <mishraalok2130@gmail.com>
2026-01-14 17:11:59 +0530

    add neon db

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import env from "dotenv";
+const { Pool } = pg;
 
 env.config();
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
-db.connect();
+
 
 const app = express();
 const port = 3000;
